7c2c2f290e7383f018f4f2f790ef3c84c88fcde2
sakura-0405 <godson950405@gmail.com>
2026-03-30 19:08:59 +0800

    版本4
    新增<聯絡我>CSS樣式
    修改Link用法
    刪除a 超連結用法
    修改<關於我>排版

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,48 +1,41 @@
-import Image from "next/image"
+import Link from "next/link";
+import styles from '@/styles/contact.module.css';
 
 export default function Page() {
   return (
     <main className="main">
-      <header className="header">
-        蝦
-      </header>
+    <header>
+      <span className={styles.header}>聯絡我</span>
 
-    <div className="Profile">
-      <table className="profile_TB">
-        <tbody>
-          <tr>
-            <td>
-              <img src="/MyPic.jpg" className="MyPic1"/>
-            </td>
-            <td className="td1">
-              <h1>蝦?</h1>
-              <span className="Name1"><a href="https://www.instagram.com/sakura._.6896_">赤櫻櫻</a></span>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+      <span className={styles.Go_Main1}>
+      <Link className={`${styles.Go_Main2} ${styles.Links}`} href="/">首頁</Link>
 
-    <div className="Links">
-      <table className="link_TB">
+      </span>
+    </header>
+    <div className={styles.Links}>
+      <table className={styles.link_TB}>
 		    <tbody>
           <tr>
-			      <td className="discord_link">
-				      <strong className="DC1">Discord</strong> : <a href="https://discord.gg/WwGGnV5D3s" target="_blank" rel="noopener noreferrer"><strong className="P1">豬玀G公園 (server)</strong></a>
-			        </td>
-			      </tr>
-			      <tr>
-			        <td className="twitch_link">
-			          <strong className="TC1">Twitch</strong> : <a href="https://www.twitch.tv/sakura_0405" target="_blank" rel="noopener noreferrer"><strong className="P2">sakura_0405</strong></a>
-			        </td>
-			      </tr>
-          </tbody>
+			      <td className={styles.link_TD}>
+              <strong className={styles.discord}>Discord : </strong>
+              <Link href="https://discord.gg/PrabXwZtQY" className={`${styles.discord_link} ${styles.Links}`}>豬玀G公園 </Link>
+			      </td>
+			    </tr>
+			    <tr>
+			      <td className={styles.link_TD}>
+              <strong className={styles.twitch}>Twitch : </strong>
+              <Link href="https://www.twitch.tv/sakura_0405" className={`${styles.twitch_link} ${styles.Links}`}>sakura_0405</Link>
+			      </td>
+			    </tr>
+          <tr>
+			      <td className={styles.link_TD}>
+              <strong className={styles.instagram}>instagram : </strong>
+              <Link href="https://www.instagram.com/sakura._.6896_/" className={`${styles.instagram_link} ${styles.Links}`}>sakura._.6896_</Link>
+			      </td>
+			    </tr>
+        </tbody>
 	    </table>
     </div>
-
-
-
-
     </main>
   )
 }
